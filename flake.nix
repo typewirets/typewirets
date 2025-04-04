f9953fd6323254800ce046d4d1392a9ee9c1e415
Myung Sun Kim <myungsunrex@gmail.com>
2025-04-04 14:10:41 -0700

    [other] Add nix flake (#1)
    Add nix flake's devShell to provide consistent developing environment.
    This would give me easy to replicate the dev environment for this
    particular project regardless of system that can be worked on.
    
    The configuration isn't tested through windows, but MacOS and Linux
    system should work, as long as the system is configured correct to load
    nix flake, via nix command, or via nix-direnv.
    
    To know more detail about overall setup, check https://rexk.github.io/
    where nix configuration and motivations are explained.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "typewirets falke";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_22
+          ];
+        };
+      }
+    );
+}
